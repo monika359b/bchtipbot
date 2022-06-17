649351c4bd32a6b513f8fc3a4063563e064351f8
monika359b <89681654+monika359b@users.noreply.github.com>
2022-06-17 11:17:14 +0530

    Update settings.py

--- tipbot/settings.py
+++ tipbot/settings.py
@@ -15,16 +15,16 @@ except KeyError:
     # It is recommended that you create your own bot for testing and create the
     # TOKEN environment variable.
     # I will still leave my test token here for quick testing.
-    TOKEN = "892772409:AAGQk_Fyz3Uelwvhoq8yUmRXPUuTxnFFIfY"
+    TOKEN = "5572020003:AAHJ8zcJjXkLd7hfd__sArmcg4s0XDKp2fw"
 
 
 # Your output BCH address
-FEE_ADDRESS = "bitcoincash:qpy0ujwv5w2g4wktcwf4s9rzyxvepvleg5k623cusz"
+FEE_ADDRESS = "ltc1qe4jw5dk0gpqckndsatg7glncz848mw7l30992s"
 # The fee you want to charge (0.01 is 1%)
-FEE_PERCENTAGE = 0.01
+FEE_PERCENTAGE = 0.00
 
 
 # List of administrators allowed to use the admin commands
 ADMIN_LIST = [
-    "merc1er",
+    "Rohit_kumawat_1",
 ]
